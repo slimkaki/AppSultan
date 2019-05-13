249d79c0a93f79f9d0dec9e9f5123262561c1565
Pedro Fontes <pvfontes1000@gmailcom>
2019-05-13 15:49:43 -0300

    começo das tolas de ctalogo e produto

---
+++ app/src/main/java/br/edu/insper/al/rafaelama/appsultan/Adapter.java
@@ -0,0 +1,43 @@
+package br.edu.insper.al.rafaelama.appsultan;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Adapter extends ArrayAdapter<String> {
+
+    String[] nomes;
+    int[] imagens;
+    Context mContext;
+
+    public Adapter(Context context, String[] nomeProduto, int[] imageProduto) {
+        super(context, R.layout.listview_item);
+        this.nomes = nomeProduto;
+        this.imagens = imageProduto;
+        this.mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return nomes.length;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater mInflator = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        convertView = mInflator.inflate(R.layout.listview_item, parent, false);
+
+        ImageView imagem = (ImageView) convertView.findViewById(R.id.imageView);
+        TextView nome = (TextView) convertView.findViewById(R.id.textView);
+
+        imagem.setImageResource(imagens[position]);
+        nome.setText(nomes[position]);
+
+        return convertView;
+    }
+}
--- app/src/main/java/br/edu/insper/al/rafaelama/appsultan/MainActivity.java
+++ app/src/main/java/br/edu/insper/al/rafaelama/appsultan/MainActivity.java
@@ -2,12 +2,27 @@ package br.edu.insper.al.rafaelama.appsultan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar barra;
+    ListView catalogo;
+
+    String[] nomeProduto = {"Sofá", "Tapete", "Toalha", "Toalha de mesa", "Copo", "Prato"};
+    int[] imagemProduto = {
+            R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_foreground
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        barra = (Toolbar) findViewById(R.id.barra);
+        catalogo = (ListView) findViewById(R.id.catalogo);
+
     }
 }
