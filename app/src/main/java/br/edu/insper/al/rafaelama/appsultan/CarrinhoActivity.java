9c458de8b21ab42c07979b139f66537cd4325571
slimkaki <rafaalmada98@gmail.com>
2019-05-14 15:15:58 -0300

    tela de carrinho (não terminada)

--- app/src/main/java/br/edu/insper/al/rafaelama/appsultan/CarrinhoActivity.java
+++ app/src/main/java/br/edu/insper/al/rafaelama/appsultan/CarrinhoActivity.java
@@ -1,14 +1,48 @@
 package br.edu.insper.al.rafaelama.appsultan;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class CarrinhoActivity extends AppCompatActivity {
+
+    private ImageButton backButton = findViewById(R.id.backButton);
+    private Button buttonCancel = findViewById(R.id.buttonCancel);
+    private Button buttonConfirma = findViewById(R.id.buttonConfirm);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carrinho);
 
+        backButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CarrinhoActivity.this, MainActivity.class);
+                setResult(2, intent);
+                finish();
+            }
+        });
+
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CarrinhoActivity.this, MainActivity.class);
+                setResult(2, intent);
+                finish();
+            }
+        });
+
+        buttonConfirma.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
