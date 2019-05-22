5fb681a780ae5a331bd232884b11e2a55669da76
4rfel <38050598+4rfel@users.noreply.github.com>
2019-05-21 23:18:27 -0300

    singup chrashando
    esta chrashando quando chama o firebase e n sei resolver

--- app/src/main/java/br/edu/insper/al/rafaelama/appsultan/SignUpActivity.java
+++ app/src/main/java/br/edu/insper/al/rafaelama/appsultan/SignUpActivity.java
@@ -2,27 +2,92 @@ package br.edu.insper.al.rafaelama.appsultan;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class SignUpActivity extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class SignUpActivity extends AppCompatActivity implements ValueEventListener {
 
     protected Button buttonCancela;
     protected Button buttonInscreve;
+
+    private TextView name, email, cpf, password, password_confirmation, celular, cep, address, numero;
+
+    private int id;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        id = 0;
+
+        name = findViewById(R.id.name_singup);
+        email = findViewById(R.id.email_singup);
+        cpf = findViewById(R.id.cpf_singup);
+        password = findViewById(R.id.password_singup);
+        password_confirmation = findViewById(R.id.password_singup_confirmation);
+        celular = findViewById(R.id.celphone_singup);
+        cep = findViewById(R.id.cep_singup);
+        address = findViewById(R.id.address_singup);
+        numero = findViewById(R.id.number_singup);
+
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        DatabaseReference referenceA_name = database.getReference(String.valueOf(id)+"/name");
+        DatabaseReference referenceA_email = database.getReference(String.valueOf(id)+"/email");
+        DatabaseReference referenceA_cpf = database.getReference(String.valueOf(id)+"/cpf");
+        DatabaseReference referenceA_password = database.getReference(String.valueOf(id)+"/password");
+        DatabaseReference referenceA_celphone = database.getReference(String.valueOf(id)+"/celphone");
+        DatabaseReference referenceA_cep = database.getReference(String.valueOf(id)+"/cep");
+        DatabaseReference referenceA_address = database.getReference(String.valueOf(id)+"/address");
+        DatabaseReference referenceA_numero = database.getReference(String.valueOf(id)+"/numero");
+
         buttonCancela = findViewById(R.id.buttonCancel);
         buttonInscreve = findViewById(R.id.singup_button);
 
         buttonInscreve.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
+
+                String nameString = name.getText().toString();
+                String emailString = email.getText().toString();
+                String cpfString = cpf.getText().toString();
+                String passwordString = password.getText().toString();
+                String passwordConfirmationString = password_confirmation.getText().toString();
+                String celularString = celular.getText().toString();
+                String cepString = cep.getText().toString();
+                String addressString = address.getText().toString();
+                String numeroString = numero.getText().toString();
+
+                // O método setValue aceita qualquer objeto e tenta
+                // deduzir qual é o tipo que deve ser escrito no banco.
+                // Nem sempre consegue, então cuidado com exceptions.
+                if(passwordConfirmationString.equals(passwordString)){
+                    referenceA_name.setValue(nameString);
+                    referenceA_email.setValue(emailString);
+                    referenceA_cpf.setValue(cpfString);
+                    referenceA_password.setValue(passwordString);
+                    referenceA_celphone.setValue(celularString);
+                    referenceA_cep.setValue(cepString);
+                    referenceA_address.setValue(addressString);
+                    referenceA_numero.setValue(numeroString);
+                }
+                else {
+
+                }
+                id++;
+
                 Intent returnIntent = new Intent(SignUpActivity.this, MainActivity.class);
                 startActivityForResult(returnIntent, 1);
             }
@@ -35,4 +100,14 @@ public class SignUpActivity extends AppCompatActivity {
             }
         });
 }
+
+    @Override
+    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+    }
+
+    @Override
+    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+    }
 }
