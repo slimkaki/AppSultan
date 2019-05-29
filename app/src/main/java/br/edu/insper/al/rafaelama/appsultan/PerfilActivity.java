9da801477765f32a492ecd2d0316626f6e110588
slimkaki <rafaalmada98@gmail.com>
2019-05-29 18:00:47 -0300

    metade da issue de perfil feita

--- app/src/main/java/br/edu/insper/al/rafaelama/appsultan/PerfilActivity.java
+++ app/src/main/java/br/edu/insper/al/rafaelama/appsultan/PerfilActivity.java
@@ -2,23 +2,32 @@ package br.edu.insper.al.rafaelama.appsultan;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class PerfilActivity extends AppCompatActivity {
+
+    TextView nameText, emailText, addressText;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
 
-        Button btaddress = findViewById(R.id.AddressButton);
-        Button btedit = findViewById(R.id.EditButton);
-        Button btrequests = findViewById(R.id.RequestsButton);
-        Button btlucro = findViewById(R.id.LucroButton);
-
+        Button btEdit = findViewById(R.id.editButton);
+        Button btLucro = findViewById(R.id.lucroButton);
         Button voltar = findViewById(R.id.voltar);
 
         ImageButton perfil = findViewById(R.id.buttonProfile);
@@ -26,10 +35,37 @@ public class PerfilActivity extends AppCompatActivity {
         ImageButton catalogo = findViewById(R.id.buttonCat);
         ImageButton pedidos = findViewById(R.id.buttonRequests);
 
-        btaddress.setOnClickListener(AddressHandler);
-        btedit.setOnClickListener(EditHandler);
-        btrequests.setOnClickListener(RequestsHandler);
-        btlucro.setOnClickListener(LucroHandler);
+        btEdit.setOnClickListener(EditHandler);
+        btLucro.setOnClickListener(LucroHandler);
+
+        nameText = findViewById(R.id.nameText);
+        emailText = findViewById(R.id.emailText);
+        addressText = findViewById(R.id.addressButton);
+
+        FirebaseUser currentFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = currentFirebaseUser.getUid();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference uidRef = databaseReference.child("users").child(uid);
+
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String name = dataSnapshot.child("name").getValue(String.class);
+                String email = dataSnapshot.child("email").getValue(String.class);
+                String address = dataSnapshot.child("address").getValue(String.class);
+
+                nameText.setText(name);
+                emailText.setText(email);
+                addressText.setText(address);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+
+        uidRef.addListenerForSingleValueEvent(valueEventListener);
 
         voltar.setOnClickListener(new View.OnClickListener() {
 
@@ -62,25 +98,19 @@ public class PerfilActivity extends AppCompatActivity {
             }
         });
 
-    }
-    View.OnClickListener AddressHandler = new View.OnClickListener(){
-        public void onClick(View v){
 
-        }
-    };
+    }
+
     View.OnClickListener EditHandler = new View.OnClickListener(){
         public void onClick(View v){
-
+            // abre a intent de edição de infos
         }
     };
-    View.OnClickListener RequestsHandler = new View.OnClickListener(){
-        public void onClick(View v){
 
-        }
-    };
     View.OnClickListener LucroHandler = new View.OnClickListener(){
         public void onClick(View v){
-
+            // Intent pra activity de profit
+           // Intent editLucro = new Intent(PerfilActivity.this, )
         }
     };
 }
