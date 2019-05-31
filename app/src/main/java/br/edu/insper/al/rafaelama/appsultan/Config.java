71c9c136214ea4526a236c13dff894c884d429fc
João Meirelles <joaopmjm@al.insper.edu.br>
2019-05-31 19:44:36 -0300

    Email funfando

---
+++ app/src/main/java/br/edu/insper/al/rafaelama/appsultan/Config.java
@@ -0,0 +1,6 @@
+package br.edu.insper.al.rafaelama.appsultan;
+
+public class Config {
+    public static final String EMAIL ="pedidossultan@gmail.com"; //your-gmail-username
+    public static final String PASSWORD ="SenhaSultan"; //your-gmail-password
+}
